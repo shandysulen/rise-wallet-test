343f17b304f3f48fd21a03b3dec4d9bae0f33915
Shandy Sulen <shandysulen@gmail.com>
2022-10-05 13:56:11 -0400

    fix formatter

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,1 @@
-export const getFormattedAddress = (address: string) => address.slice(0, 7) + '...' + address.slice(-5);
+export const getFormattedAddress = (address: string) => address.slice(0, 7) + '...' + address.slice(-4);
